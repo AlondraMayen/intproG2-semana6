f52a773f251c2f6328652f3b1e045c8c38349ff5
AlondraMayen <asmayen@uamv.edu.ni>
2025-04-29 11:27:26 -0600

    ejercicio4

---
+++ for/for4.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Ingrese el primer numero: "))
+numero2 = int(input("Ingrese el segundo numero: "))
+multiplicacion = numero1 * numero2
+
+print ("El resultrado de la multiplicacion es: ", multiplicacion)
+
