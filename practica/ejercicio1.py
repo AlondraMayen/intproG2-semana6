ee85989acd85ccc08965e7ba5d6a49739c8adaeb
AlondraMayen <asmayen@uamv.edu.ni>
2025-04-30 18:40:13 -0600

    practica1

---
+++ practica/ejercicio1.py
@@ -0,0 +1,8 @@
+#Intrecambiar el valor de dos variables
+a = int(input("Ingrese el valor de a: "))
+b = int(input("Ingrese el valor de b: "))
+
+a , b = b , a #asi deben ser para que no haya error
+
+print("El nuevo valor de a es: ", a , "El nuevo valor de b es: ", b)
+
