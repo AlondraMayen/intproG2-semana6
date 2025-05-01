e4ab490893137208cdd9ef1b4bde2dc84dedb80d
AlondraMayen <asmayen@uamv.edu.ni>
2025-04-30 18:42:43 -0600

    practica6

---
+++ practica/ejercicio6.py
@@ -0,0 +1,9 @@
+# Programa que pida un caracter e indique si es una vocal o no
+
+caracter = input("Ingrese un caracter (a-z): ")
+
+if caracter == 'a'or 'e' or 'i' or 'o' or 'u':
+    print ("Es una vocal")
+
+else:
+    print ("No es una vocal")
